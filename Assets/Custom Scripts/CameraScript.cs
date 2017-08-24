56481ac6cee9e14b0659eae5abe6d7d5719e8a03
Charles Lu <kuanminglu0529@gmail.com>
2017-08-24 12:45:09 +0200

    initial commit. player movement using transform translate and space to jump. getaxisraw for movement. colliders on walls, ground and player. timer used for high jump, colour change in player to show high jump.

---
+++ Assets/Custom Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject Player;
+	public float CameraSmoothTime = 5f;
+	Vector3 offset;
+	Vector3 cameraVelocity = Vector3.zero;
+	Vector3 playerPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset = transform.position - new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+		playerPosition  = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+		transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref cameraVelocity, CameraSmoothTime);
+
+		/*if(offset.x > 3f || offset.x < -3f){
+			transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref cameraVelocity, CameraSmoothTime);
+		}*/
+		
+	}
+}
---
+++ Assets/Custom Scripts/PlayerScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float JumpSpeed = 5f;
+	public float MoveSpeed = 2f;
+	public Color MaxJumpColor;
+	bool isInAir = false;
+	float timeHeld = 0f;
+	bool maxJump = false;
+	Color originalColor;
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		originalColor = GetComponent<SpriteRenderer>().color;
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!isInAir){
+			if( Input.GetKeyUp("space") ){
+				Jump();
+				isInAir = true;
+			}
+		}
+
+		if( Input.GetKey("space") ){
+			timeHeld += Time.deltaTime;
+		}else{
+			timeHeld = 0;
+		}
+
+		if(timeHeld > 1){
+			maxJump = true;
+		}else{
+			maxJump = false;
+		}
+
+		if(maxJump){
+			GetComponent<SpriteRenderer>().color = MaxJumpColor;
+		}else{
+			GetComponent<SpriteRenderer>().color = originalColor;
+		}
+
+		rb.velocity = new Vector2(0,rb.velocity.y);
+			
+
+		transform.Translate(Vector3.right* Time.deltaTime * MoveSpeed * Input.GetAxisRaw("Horizontal"));
+	}
+
+	void OnCollisionEnter2D(Collision2D col2D){
+		isInAir = false;
+	}
+
+	void OnCollisionStay(Collision collisionInfo) {
+		
+	}
+
+	void Jump(){
+		if(maxJump){
+			rb.velocity = transform.up * JumpSpeed * 1.2f;
+		}else{
+			rb.velocity = transform.up * JumpSpeed * 0.7f;
+		}
+		
+	}
+
+}
