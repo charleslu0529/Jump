e39792cb453f3b4938a6501af83fe215eaa6f276
Charles Lu <kuanminglu0529@gmail.com>
2017-08-24 15:58:41 +0200

    wall slide added using ray cast. ray cast left to stop staggering movement. default jump overrided in unity to 5. Tags added to walls. boolean added for wall jump.

--- Assets/Custom Scripts/PlayerScript.cs
+++ Assets/Custom Scripts/PlayerScript.cs
@@ -7,12 +7,16 @@ public class PlayerScript : MonoBehaviour {
 
 	public float JumpSpeed = 5f;
 	public float MoveSpeed = 2f;
+	public float MaxJumpMultiplier = 1.3f;
 	public Color MaxJumpColor;
 	bool isInAir = false;
 	float timeHeld = 0f;
 	bool maxJump = false;
+	bool canWallJump = false;
 	Color originalColor;
 	Rigidbody2D rb;
+	RaycastHit2D castLeft;
+	RaycastHit2D castRight;
 	// Use this for initialization
 	void Start () {
 		originalColor = GetComponent<SpriteRenderer>().color;
@@ -50,21 +54,53 @@ public class PlayerScript : MonoBehaviour {
 		rb.velocity = new Vector2(0,rb.velocity.y);
 			
 		rb.velocity = new Vector3(MoveSpeed * Input.GetAxisRaw("Horizontal"), rb.velocity.y,0f);
+
+		castLeft = Physics2D.Raycast(transform.position + new Vector3(-0.55f,0,0), Vector3.left, 0.1f);
+		castRight = Physics2D.Raycast(transform.position + new Vector3(0.55f,0,0), Vector3.right, 0.1f);
+        if(castLeft.collider != null){
+        	if(rb.velocity.x < 0){
+        		rb.velocity = new Vector3(0, rb.velocity.y, 0f);
+        	}
+        }
+
+        if(castRight.collider != null){
+        	if(rb.velocity.x < 0){
+        		rb.velocity = new Vector3(0, rb.velocity.y, 0f);
+        	}
+        }
+
+		Debug.DrawRay(transform.position + new Vector3(-0.55f,0,0), Vector3.left,Color.green);
+
 	}
 
 	void OnCollisionEnter2D(Collision2D col2D){
 		isInAir = false;
 	}
 
-	void OnCollisionStay(Collision collisionInfo) {
-		
-	}
+	void OnCollisionStay2D(Collision2D col2D) {
+        
+        RaycastHit2D castDown = Physics2D.Raycast(transform.position + new Vector3(0,-0.5f,0), Vector3.down, 0.1f);
+        if(castDown.collider == null){
+        	rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y/1.5f, 0f);
+        	canWallJump = true;
+        }else{
+        	canWallJump = false;
+        }
+        
+        Debug.Log(castDown);
+    }
+
+   
 
 	void Jump(){
 		if(maxJump){
-			rb.velocity = transform.up * JumpSpeed * 1.2f;
+			if(canWallJump){
+				rb.velocity = new Vector3(1,1,0).normalized * JumpSpeed * MaxJumpMultiplier;
+			}else{
+				rb.velocity = transform.up * JumpSpeed * MaxJumpMultiplier;
+			}
 		}else{
-			rb.velocity = transform.up * JumpSpeed * 0.7f;
+			rb.velocity = transform.up * JumpSpeed;
 		}
 		
 	}
